7585880859ce0924f9126118bc2f4f5f0364de42
Terry Zheng <143101063+terryzg@users.noreply.github.com>
2024-11-02 16:13:19 -0400

    Add files via upload

---
+++ HACKNC 2024/content.js
@@ -0,0 +1,6 @@
+// src/content/content.js
+
+// This script will run on all matched pages
+console.log("Content script is running!");
+// Example: Modify the page in some way
+document.body.style.backgroundColor = "#f0f0f0"; // Change the background color
